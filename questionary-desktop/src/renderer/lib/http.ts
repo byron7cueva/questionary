d80b3ce980f1f89a27c90e38b6f8937c89b59938
byron7cueva <byron7cueva@gmail.com>
2020-09-29 10:52:40 -0500

    Fiexed lint on htt service

--- questionary-desktop/src/renderer/lib/http.ts
+++ questionary-desktop/src/renderer/lib/http.ts
@@ -15,58 +15,42 @@ class HttpService {
     return HttpService.instance;
   }
 
-  async get(endPoint: string) {
-    try {
-      const request =  await fetch(`${this.url}${endPoint}`);
-      const data = await request.json();
-      return data;
-    } catch (error) {
-      throw error;
-    }
+  async get(endPoint: string): Promise<unknown> {
+    const request =  await fetch(`${this.url}${endPoint}`);
+    const data = await request.json();
+    return data;
   }
 
-  async post(endPoint: string, body: any) {
-    try {
-      const request = await fetch(`${this.url}${endPoint}`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(body)
-      });
-      const data = await request.json();
-      return data;
-    } catch (error) {
-      throw error;
-    }
+  async post(endPoint: string, body: unknown): Promise<unknown> {
+    const request = await fetch(`${this.url}${endPoint}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    });
+    const data = await request.json();
+    return data;
   }
 
-  async put(endPoint: string, id: string, body: any) {
-    try {
-      const request = await fetch(`${this.url}${endPoint}/${id}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(body)
-      });
-      const data = await request.json();
-      return data;
-    } catch (error) {
-      throw error;
-    }
+  async put(endPoint: string, id: string, body: unknown): Promise<unknown> {
+    const request = await fetch(`${this.url}${endPoint}/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    });
+    const data = await request.json();
+    return data;
   }
 
-  async delete(endPoint: string, id: string) {
-    try {
-      const request = await fetch(`${this.url}${endPoint}/${id}`, {
-        method: 'DELETE'
-      });
-      const data = request.json();
-      return data;
-    } catch (error) {
-      throw error;
-    }
+  async delete(endPoint: string, id: string): Promise<unknown> {
+    const request = await fetch(`${this.url}${endPoint}/${id}`, {
+      method: 'DELETE'
+    });
+    const data = request.json();
+    return data;
   }
 }
 
