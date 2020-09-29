5b378130a47bccb44d69bf71f3b4b56a76769aed
byron7cueva <byron7cueva@gmail.com>
2020-09-29 10:54:01 -0500

    Fixed lint on main.ts

--- questionary-desktop/src/main.ts
+++ questionary-desktop/src/main.ts
@@ -3,7 +3,7 @@ import { app, BrowserWindow } from 'electron';
 import { setupMenu } from './main/menu';
 import { setupMessageDialog } from './main/messageDialog';
 
-declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
+declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
