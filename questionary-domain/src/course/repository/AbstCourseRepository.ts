66adfb672d74bb37155acdb43b1affb3230cba05
byron7cueva <byron7cueva@gmail.com>
2020-10-01 12:24:05 -0500

    Add abstract methods to implements in AbstCouseRepository

--- questionary-domain/src/course/repository/AbstCourseRepository.ts
+++ questionary-domain/src/course/repository/AbstCourseRepository.ts
@@ -1,5 +1,5 @@
 import { AbstRepository } from 'questionary-common';
-import { Course } from '../entity/Course';
+import { Course, CourseCreate } from '../entity/Course';
 
 /**
  * Abstract class to implements CourseRepository
@@ -12,4 +12,35 @@ export abstract class AbstCourseRepository extends AbstRepository {
    * @return {Promise<Course[]>} List of courses
    */
   abstract getAll(): Promise<Course[]>;
+
+  /**
+   * Create the new course
+   * 
+   * @param {CourseCreate} course Interface to create course
+   * @return {Course} Course saved
+   */
+  abstract create(course: CourseCreate): Course;
+
+  /**
+   * Update course
+   * 
+   * @param {Course} course Course to update
+   * @return {Course} Course updated
+   */
+  abstract update(course: Course): Course;
+
+  /**
+   * Delete course
+   * 
+   * @param {number} courseId Id of course
+   * @return {boolean} True if course deleted or false if don't deleted
+   */
+  abstract delete(courseId: number): boolean;
+
+  /**
+   * Get one course by courseId
+   * 
+   * @param {number} courseId Id of course
+   */
+  abstract getById(courseId: number): Course;
 }
