8de09876bc5a6e0e087fdeb3ff104fa9b93223b4
byron7cueva <byron7cueva@gmail.com>
2020-09-30 21:14:09 -0500

    Add abstrac class App factory

---
+++ questionary-application/src/app/AbstApp.ts
@@ -0,0 +1,54 @@
+import { DependencyContainer, container, InjectionToken } from "tsyringe";
+
+import { ContainerToken } from './ContainerToken';
+import { CourseUseCase, CourseGateway } from '../course';
+
+/**
+ * Abstract App
+ * Fatory for create artefacts
+ */
+export abstract class AbstApp {
+
+  /**
+   * Container
+   */
+  private _container: DependencyContainer = container;
+
+  /**
+   * Constructor of AbstApp
+   */
+  constructor() {
+    this.registerRepositories();
+    this.registerUseCase();
+  }
+
+  /**
+   * Return container
+   */
+  protected get container(): DependencyContainer {
+    return this._container;
+  }
+
+  /**
+   * Register Repository instances
+   */
+  abstract registerRepositories(): void;
+
+  /**
+   * Register UseCase instances
+   */
+  private registerUseCase() {
+    this.container.register(ContainerToken.getInstance().AbstCourseUseCase, {
+      useClass: CourseUseCase
+    });
+  }
+
+  /**
+   * Get of container instance
+   * 
+   * @return {CourseGateway} Intance of CourseGateway
+   */
+  getCourseGateway(): CourseGateway {
+    return this.container.resolve(CourseGateway);
+  }
+}
