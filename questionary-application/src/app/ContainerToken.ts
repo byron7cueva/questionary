34a32f31624d46ee4446dbcfd69a61a25797212c
byron7cueva <byron7cueva@gmail.com>
2020-09-30 21:19:22 -0500

    Add resolve tokens container class

---
+++ questionary-application/src/app/ContainerToken.ts
@@ -0,0 +1,51 @@
+/**
+ * Singleton for resolve tokens of instance register on container
+ */
+export class ContainerToken {
+
+  /**
+   * Instance of ContainerToken
+   * 
+   * @private
+   * @static
+   */
+  private static instance: ContainerToken;
+
+  /**
+   * Private constructor
+   * 
+   * @private
+   */
+  private constructor(){}
+
+  /**
+   * Return instance of ContainerToken
+   * 
+   * @static
+   * @return {ContainerToken} Instance
+   */
+  static getInstance(): ContainerToken {
+    if(!ContainerToken.instance) {
+      ContainerToken.instance = new ContainerToken();
+    }
+    return ContainerToken.instance;
+  }
+
+  /* Repositories */
+
+  /**
+   * Return token of AbstCourseRepository
+   * 
+   * @return {string} Token
+   */
+  get AbstCourseRepository(): string { return 'AbstCourseRepository' };
+
+  /* UseCase */
+
+  /**
+   * Return token of AbstCourseUseCase
+   * 
+   * @return {string} Token
+   */
+  get AbstCourseUseCase(): string { return 'AbstCourseUseCase' };
+}
