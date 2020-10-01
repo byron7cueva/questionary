ecebee37767bf4c66ab5c10dab31ba5c214458a3
byron7cueva <byron7cueva@gmail.com>
2020-09-30 21:27:44 -0500

    Add implementation App factory

---
+++ questionary-infrastructure/src/app/App.ts
@@ -0,0 +1,47 @@
+import { AbstApp, ContainerToken } from 'questionary-application';
+
+import { CourseRepository } from '../repository/CourseRepository';
+
+/**
+ * Singleton App factory implementation
+ */
+export class App extends AbstApp {
+
+  /**
+   * Instance of App
+   * 
+   * @static
+   * @private
+   */
+  private static instance: App;
+
+  /**
+   * Constructor of App
+   * 
+   * @private
+   */
+  private constructor() {
+    super();
+  }
+
+  /**
+   * Return instance of App
+   * 
+   * @return {App} Instance
+   */
+  static getInstance(): App {
+    if (!App.instance) {
+      App.instance = new App();
+    }
+    return App.instance;
+  }
+
+  /**
+   * Register the repository artefacts
+   */
+  registerRepositories(): void {
+    this.container.register(ContainerToken.getInstance().AbstCourseRepository, {
+      useClass: CourseRepository
+    });
+  }
+}
