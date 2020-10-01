91925c0089589301d0783bb5daa4ece5574be759
byron7cueva <byron7cueva@gmail.com>
2020-10-01 18:43:37 -0500

    Create and register questions router

---
+++ questionary-infrastructure/src/routes/questions.ts
@@ -0,0 +1,15 @@
+import { Router, Request, Response, NextFunction } from 'express';
+
+import { App } from '../app/App';
+
+const questionsRouter = Router();
+
+/* Routes */
+
+
+/* Implements routes */
+
+
+export {
+  questionsRouter
+}
--- questionary-infrastructure/src/server/index.ts
+++ questionary-infrastructure/src/server/index.ts
@@ -1,9 +1,11 @@
 import { Server } from './Server';
 
 import { coursesRouter } from '../routes/courses';
+import { questionsRouter } from '../routes/questions';
 
 /* Register routes on server */
 Server.getInstance().registerRoute('/courses', coursesRouter);
+Server.getInstance().registerRoute('/questions', questionsRouter);
 
 export {
   Server
