cfb4960d009335d2c78caaa18fde829ae691e331
byron7cueva <byron7cueva@gmail.com>
2020-09-30 20:34:28 -0500

    Add comments to entities

--- questionary-domain/src/course/entity/Course.ts
+++ questionary-domain/src/course/entity/Course.ts
@@ -1,8 +1,14 @@
 import { Optional } from 'questionary-common';
 
+/**
+ * Entity Course
+ */
 export interface Course {
   courseId: number;
   name: string;
 }
 
+/**
+ * Entity for create course with con optional courseId
+ */
 export interface CourseCreate extends Optional<Course, 'courseId'> {}
--- questionary-domain/src/question/entity/Question.ts
+++ questionary-domain/src/question/entity/Question.ts
@@ -1,5 +1,8 @@
 import { Optional } from 'questionary-common';
 
+/**
+ * Question Entity
+ */
 export interface Question {
   questionId: number;
   courseId: number;
@@ -7,4 +10,7 @@ export interface Question {
   answere: string;
 }
 
+/**
+ * Entity for create Question
+ */
 export interface QuestionCreate extends Optional<Question, 'questionId'> {}
