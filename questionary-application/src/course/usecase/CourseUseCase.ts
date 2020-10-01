eeb81df7eaaf3751fbcb2ea773aa0e53bcdd478c
byron7cueva <byron7cueva@gmail.com>
2020-10-01 15:43:58 -0500

    Implements save and update in Course use case

--- questionary-application/src/course/usecase/CourseUseCase.ts
+++ questionary-application/src/course/usecase/CourseUseCase.ts
@@ -3,7 +3,7 @@ import {
   ContainerToken,
   AbstCourseUseCase,
   AbstCourseRepository,
-  Course
+  Course, CourseCreate
 } from 'questionary-domain';
 
 /**
@@ -11,7 +11,6 @@ import {
  */
 @injectable()
 export class CourseUseCase extends AbstCourseUseCase {
-
   /**
    * Constructor of CourseUseCase
    * 
@@ -30,5 +29,32 @@ export class CourseUseCase extends AbstCourseUseCase {
    */
   getAll(): Promise<Course[]> {
     return this.repository.findAll();
+  }
+
+  /**
+   * Save a new course
+   * 
+   * @param {CourseCreate} course Course to save
+   * @return {Promise<Course>} Course saved
+   */
+  save(course: CourseCreate): Promise<Course> {
+    return this.repository.create(course);
+  }
+
+  /**
+   * Update course
+   * 
+   * @param {Course} course Course to update
+   * @return {Promise<Course>} Course updated
+   */
+  update(course: Course): Promise<Course> {
+    return this.repository.update(course);
+  }
+
+  delete(courseId: number): Promise<boolean> {
+    throw new Error('Method not implemented.');
+  }
+  getCourseAndQuestionsById(courseId: number): Promise<Course> {
+    throw new Error('Method not implemented.');
   }
 }
--- questionary-domain/src/course/usecase/AbstCourseUseCase.ts
+++ questionary-domain/src/course/usecase/AbstCourseUseCase.ts
@@ -1,6 +1,7 @@
 import { AbstUseCase } from 'questionary-common';
+
 import { AbstCourseRepository } from '../repository/AbstCourseRepository';
-import { Course } from '../entity/Course';
+import { Course, CourseCreate } from '../entity/Course';
 
 /**
  * Abstract class to implemented CourseUseCase
@@ -13,4 +14,24 @@ export abstract class AbstCourseUseCase extends AbstUseCase<AbstCourseRepository
    * @return {Promise<Course[]>} List of all courses
    */
   abstract getAll(): Promise<Course[]>;
+
+  /**
+   * Save a new course
+   * 
+   * @param {CourseCreate} course Course to save
+   * @return {Promise<Course>} Course saved
+   */
+  abstract save(course: CourseCreate): Promise<Course>;
+
+  /**
+   * Update course
+   * 
+   * @param {Course} course Course to update
+   * @return {Promise<Course>} Course updated
+   */
+  abstract update(course: Course): Promise<Course>;
+
+  abstract delete(courseId: number): Promise<boolean>;
+
+  abstract getCourseAndQuestionsById(courseId: number): Promise<Course>;
 }
