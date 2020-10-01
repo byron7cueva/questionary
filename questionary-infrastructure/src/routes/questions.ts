c7c6d45210016b94f933c4a7adb8970aa44d7974
byron7cueva <byron7cueva@gmail.com>
2020-10-01 18:52:28 -0500

    Implement put on questions router

--- questionary-infrastructure/src/routes/questions.ts
+++ questionary-infrastructure/src/routes/questions.ts
@@ -6,6 +6,7 @@ const questionsRouter = Router();
 
 /* Routes */
 questionsRouter.post('/', saveQuestion);
+questionsRouter.put('/:questionId', updateQuestion);
 
 
 /* Implements routes */
@@ -21,6 +22,24 @@ async function saveQuestion(request: Request, response: Response, next: NextFunc
   try {
     const { body } = request;
     const question = await App.getInstance().questionGateway.save(body);
+    response.status(201);
+    response.send(question);
+  } catch(error) {
+    next(error);
+  }
+}
+
+/**
+ * Route to update a question
+ * 
+ * @param {Request} request Request object
+ * @param {Response} response Response object
+ * @param {NextFunction} next Next function
+ */
+async function updateQuestion(request: Request, response: Response, next: NextFunction) {
+  try {
+    const { body, params: { questionId } } = request;
+    const question = await App.getInstance().questionGateway.update(questionId, body);
     response.status(200);
     response.send(question);
   } catch(error) {
