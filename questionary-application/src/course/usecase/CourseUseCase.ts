7c3fa9f10964c71edf057d1c50ed7cb4cfb2ea48
byron7cueva <byron7cueva@gmail.com>
2020-09-30 21:00:47 -0500

    Add CouseUseCase implementarion

---
+++ questionary-application/src/course/usecase/CourseUseCase.ts
@@ -0,0 +1,30 @@
+import {injectable, inject} from 'tsyringe';
+import { AbstCourseUseCase, AbstCourseRepository, Course } from 'questionary-domain';
+import { ContainerToken } from '../../app/ContainerToken';
+
+/**
+ * Class CourseUseCase
+ */
+@injectable()
+export class CourseUseCase extends AbstCourseUseCase {
+
+  /**
+   * Constructor of CourseUseCase
+   * 
+   * @param {AbstCourseRepository} repository Repository
+   */
+  constructor(
+    @inject(ContainerToken.getInstance().AbstCourseRepository) repository: AbstCourseRepository
+  ) {
+    super(repository);
+  }
+
+  /**
+   * Get all Courses
+   * 
+   * @return {Promise<Course[]>} List of courses
+   */
+  getAll(): Promise<Course[]> {
+    return this.repository.getAll();
+  }
+}
