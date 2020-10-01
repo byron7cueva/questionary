1f03b49e2fcd2ef39be50552f610c8c300090b7c
byron7cueva <byron7cueva@gmail.com>
2020-10-01 18:47:33 -0500

    Implements post route in questions router

--- questionary-infrastructure/src/routes/questions.ts
+++ questionary-infrastructure/src/routes/questions.ts
@@ -5,10 +5,28 @@ import { App } from '../app/App';
 const questionsRouter = Router();
 
 /* Routes */
+questionsRouter.post('/', saveQuestion);
 
 
 /* Implements routes */
 
+/**
+ * Route to save a question
+ * 
+ * @param {Request} request Request object
+ * @param {Response} response Response object
+ * @param {NextFunction} next Next function
+ */
+async function saveQuestion(request: Request, response: Response, next: NextFunction) {
+  try {
+    const { body } = request;
+    const question = await App.getInstance().questionGateway.save(body);
+    response.status(200);
+    response.send(question);
+  } catch(error) {
+    next(error);
+  }
+}
 
 export {
   questionsRouter
