138b8f875b8565361fa113ac872f894e06197322
byron7cueva <byron7cueva@gmail.com>
2020-10-01 14:46:53 -0500

    Add Model to question

---
+++ questionary-infrastructure/src/model/QuestionModel.ts
@@ -0,0 +1,46 @@
+import { Model, DataTypes } from 'sequelize';
+import { Question, QuestionCreate } from 'questionary-domain';
+
+import { Connection } from '../database/Connection';
+
+/**
+ * Model to Question
+ */
+class QuestionModel extends Model<Question, QuestionCreate> implements Question {
+  questionId!: number;
+  courseId!: number;
+  question!: string;
+  answere!: string;
+}
+
+QuestionModel.init(
+  {
+    questionId: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      field: 'question_id'
+    },
+    courseId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    question: {
+      type:new DataTypes.STRING(100),
+      allowNull: false
+    },
+    answere: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
+  },
+  {
+    tableName: 'question',
+    sequelize: Connection.getInstance().connect,
+    timestamps: false
+  }
+);
+
+export {
+  QuestionModel
+}
